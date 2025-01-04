8d077ff10c4614e791bcc96fa269fec2bfe97f43
tusaki-hayama <zhengxiaoxiu5406@gmail.com>
2025-01-05 00:19:38 +0800

    update

--- 扩散模型4/工具类.py
+++ 扩散模型4/工具类.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 from torchvision import transforms
 import os
 import torch
-
+import random
 img2tensor = transforms.ToTensor()
 tensor2img = transforms.ToPILImage()
 
@@ -30,3 +30,64 @@ def shuffle_and_div_batch(image_Tensor, batch_size):
     div_image = shuffle_image[:groups * batch_size]
     group_batch_image = div_image.view((groups, batch_size, 3, 64, 64))
     return group_batch_image
+
+
+a = 0.6
+b = 0.9
+"""
+噪声添加:点噪声,线噪声,方块噪声,高斯噪声,组合噪声
+"""
+
+
+def gussi_noise(batch_size):
+    lr = 1.5 + random.random()
+    noise = torch.randn((batch_size, 1, 64, 64))
+    noise[noise <= lr] = 1
+    noise[noise > lr] = 0
+    for bs in range(batch_size):
+        if random.random() > 0.1:
+            continue
+        noise[bs] = 1
+    return noise
+    pass
+
+
+def line_noise(batch_size):
+    lr = 1 + random.random()
+    noise = torch.ones((batch_size, 1, 64, 64))
+    for bs in range(batch_size):
+        if random.random() < 0.1:
+            continue
+        p = torch.randn(64)
+        noise[bs, :, p > lr] = 0
+        p = torch.randn(64)
+        noise[bs, :, :, p > lr] = 0
+    return noise
+    pass
+
+
+def block_noise(batch_size):
+    noise = torch.ones((batch_size, 1, 64, 64))
+    for bs in range(batch_size):
+        if random.random() < 0.1:
+            continue
+        x = random.randint(0, 31)
+        y = random.randint(0, 31)
+        length = random.randint(8, 32)
+        width = random.randint(8, 32)
+        noise[bs, :, x:x + width, y:y + length] = 0
+    return noise
+
+
+"""
+"""
+
+
+def random_noise(batch_size):
+    noise = torch.ones((batch_size, 1, 64, 64))
+    noise = noise * gussi_noise(batch_size) * line_noise(batch_size)* line_noise(batch_size) * block_noise(batch_size)*block_noise(batch_size)
+    for bs in range(batch_size):
+        if random.random() < 0.1:
+            noise[bs] = 1
+    return noise
+    pass
--- 扩散模型4/扩散模型训练.py
+++ 扩散模型4/扩散模型训练.py
@@ -1,9 +1,9 @@
 import os
-
+import random
 import torch
 from torchvision import transforms
 from torch import nn
-from 工具类 import load_data, shuffle_and_div_batch, img2tensor, tensor2img
+from 工具类 import load_data, shuffle_and_div_batch, img2tensor, tensor2img, random_noise
 from 自编码器模型 import auto_encoder
 from 扩散模型 import diffusion_model
 from torch import optim
@@ -13,24 +13,44 @@ from PIL import Image
 epochs = 10000000000
 epoch = 0
 batch_size = 64
-step = 1000
+time_step = 20
+lr = 1e-4
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 auto_model = auto_encoder()
 auto_model.to(device)
 auto_model.load_state_dict(torch.load('自编码器模型/checkpoint_auto_encoder.pt'))
 auto_model.eval()
+diffusion_model = diffusion_model()
+diffusion_model.to(device)
+checkpoint_loss = 203
+checkpoint_loss = None
+checkpoint_model = '自编码器模型/checkpoint_auto_encoder.pt'
+checkpoint_model = None
+best_loss = checkpoint_loss if checkpoint_loss is not None else float('inf')
+mse_loss = nn.MSELoss(reduction='sum')
+optimizer = optim.Adam(diffusion_model.parameters(), lr=lr)
+
 f_train = (r'C:\Users\86134\Desktop\作业\0重修\神经网络深度学习'
            r'\课程项目\archive\TRAIN')
 f_val = (r'C:\Users\86134\Desktop\作业\0重修\神经网络深度学习'
          r'\课程项目\archive\VAL')
 train_data = load_data(f_train)
 val_data = load_data(f_val)
+beta = torch.linspace(0, 1, time_step)
+alpha = 1 - beta
+alpha_mul = torch.cumprod(alpha, dim=0)
+print(alpha_mul)
+while epoch < epochs:
+    epoch += 1
+    # 准备训练数据
+    data2train = shuffle_and_div_batch(train_data, random.randint(64, batch_size))
+    noise_data2train = data2train * random_noise(batch_size)
+    random_t = random.randint(0, time_step - 1)
 
+    # 训练
+    diffusion_model.train()
+    train_loss = 0
 
+    break
 
-
-
-
-
-
-
+    pass
